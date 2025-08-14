85c9d3c0437a295a27d8df1fc0bc1593b086eb4b
sanjitxdutta <duttasanjit691@gmail.com>
2025-08-14 20:24:38 +0530

    Updated Second Brain to the projects section

--- src/components/projects/ProjectData.jsx
+++ src/components/projects/ProjectData.jsx
@@ -1,32 +1,39 @@
 import ProjectImg1 from "../../assets/project1.png";
 import ProjectImg2 from "../../assets/project2.png";
 import ProjectImg3 from "../../assets/project3.png";
+import ProjectImg4 from "../../assets/project4.png";
+
 
 const ProjectData = [
   {
     id: 1,
     title: "Tomato Delivery",
-    description:
-      "Tomato Delivery, a complete food ordering solution including a Customer Frontend, Admin Dashboard, and a Backend API.",
+    description: "Tomato Delivery, a complete food ordering solution including a Customer Frontend, Admin Dashboard, and a Backend API.",
     image: ProjectImg1,
     liveLink: "https://tomato-delivery-frontend-upy2.onrender.com/",
     githubLink: "https://github.com/sanjitxdutta/tomato-delivery",
   },
   {
     id: 2,
-    title: "Eduverse API",
-    description:
-      "Eduverse API is a complete backend solution for course platforms with secure auth, role-based access, and tracking support.",
+    title: "Second Brain",
+    description: "Second Brain: Your personal link manager — save, organize & access links from LinkedIn, Twitter, YouTube, blogs & more in one place.",
     image: ProjectImg2,
+    liveLink: "https://second-brainx.vercel.app/",
+    githubLink: "https://github.com/sanjitxdutta/second-brain",
+  },
+  {
+    id: 3,
+    title: "Eduverse API",
+    description: "Eduverse API is a complete backend solution for course platforms with secure auth, role-based access, and tracking support.",
+    image: ProjectImg3,
     liveLink: "",
     githubLink: "https://github.com/sanjitxdutta/eduverse-api",
   },
   {
-    id: 3,
+    id: 4,
     title: "Todo CLI Manager",
-    description:
-      "TaskCLI is a simple, powerful Node.js tool using Commander to manage tasks — add, update, delete, and complete.",
-    image: ProjectImg3,
+    description: "TaskCLI is a simple, powerful Node.js tool using Commander to manage tasks — add, update, delete, and complete.",
+    image: ProjectImg4,
     liveLink: "",
     githubLink: "https://github.com/sanjitxdutta/todo-cli-manager",
   },
